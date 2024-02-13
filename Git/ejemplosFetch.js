a6e518d367acab826ea94ea6acb5d54dba4336fe
Jose Carlos Cárdenas Martínez <josenanodev@gmail.com>
2024-02-13 14:06:11 -0600

    errores fetch

--- Git/ejemplosFetch.js
+++ Git/ejemplosFetch.js
@@ -1,4 +1,34 @@
-fetch("https://pokeapi.co/api/v2/pokemon/ditto").then((respuesta) => {
-    console.log(respuesta);
-    return respuesta.json();
+fetch("https://pokeapi.co/api/v2/pokemonesX/ditto").then((respuesta) => {
+  respuesta.json().then((pokemon) => {
+    console.log(pokemon.order);
+  }).catch((error) => {
+    console.log("Error al obtener el pokemon", error);
+  });
+  console.log("Hola amigos");
+});
+
+fetch({
+    method: "GET",
+    url: "https://pokeapi.co/api/v2/pokemonesX/ditto",
 })
+
+//JSON: JavaScript Object Notation
+// {
+//   "nombre": "Juan",
+//   "apellido": "Perez",
+//   "edad": 30,
+//   "direccion": {
+//     "calle": "Av. Siempre Viva",
+//     "numero": 742
+//   },
+// }
+
+const objeto = {
+  nombre: "Juan",
+  apellido: "Perez",
+  edad: 30,
+  direccion: {
+    calle: "Av. Siempre Viva",
+    numero: 742,
+  },
+};
