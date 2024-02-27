1d22c135585d526180c9c999bffa3c3984d0270d
Jose Carlos Cárdenas Martínez <josenanodev@gmail.com>
2024-02-27 14:01:14 -0600

    Add new route for "/otra-cosa" and return JSON response

--- servidor/app.js
+++ servidor/app.js
@@ -1,9 +1,16 @@
 const express = require("express");
 
-const app = express();
+const app = express(); // Crear una instancia de express
 
-app.get("/", (req, res) => {
-  res.send("Como tan muchachos?");
+app.get("/otra-cosa", (request, response) => {
+  const objeto = {
+    mensaje: "63254732646239746239746",
+    cosa: "otra cosa",
+  };
+  const json = JSON.stringify(objeto);
+  return response.send(json);
 });
+
+localhost:3000/otra-cosa
 
 module.exports = app;
