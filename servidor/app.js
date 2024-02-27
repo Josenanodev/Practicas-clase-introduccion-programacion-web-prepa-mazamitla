5b9e6f0ae68f906347fdade58b0b4d1f9470a6f9
Jose Carlos Cárdenas Martínez <josenanodev@gmail.com>
2024-02-27 14:17:24 -0600

    Add dynamic route with request parameters

--- servidor/app.js
+++ servidor/app.js
@@ -2,15 +2,16 @@ const express = require("express");
 
 const app = express(); // Crear una instancia de express
 
-app.get("/otra-cosa", (request, response) => {
+app.get("/otra-cosa/:cosa/:cosaDos", (request, response) => {
   const objeto = {
     mensaje: "63254732646239746239746",
     cosa: "otra cosa",
   };
+  console.log(request.params);
   const json = JSON.stringify(objeto);
   return response.send(json);
 });
 
-localhost:3000/otra-cosa
+"localhost:3000/otra-cosa/valor/1000"
 
 module.exports = app;
