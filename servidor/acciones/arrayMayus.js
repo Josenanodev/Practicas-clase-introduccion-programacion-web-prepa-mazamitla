7add83471c4a89e4b1fe12c60ba1170fc6433435
Angelrac <157447547+Angelrac@users.noreply.github.com>
2024-03-13 14:41:54 -0600

    trabajo version definitiva
    mis presiosos commits

--- servidor/acciones/arrayMayus.js
+++ servidor/acciones/arrayMayus.js
@@ -1,7 +1,7 @@
 function mayuscularArray(array){
     const arrayguardar = []
     for (let i = 0; i < array.lenght; i++ ){
-        leerArray [i]. toUpperCase()
-        arrayNuevo[i] = leerArray [i]. toUpperCase()
+        arrayNuevo[i] = leerArray[i].toUpperCase()
     }
+    return arrayguardar 
 }
