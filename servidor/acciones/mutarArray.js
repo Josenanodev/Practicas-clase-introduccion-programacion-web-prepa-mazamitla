903a96ffb8e1d44e8ed2244f3f37afaf9ffc0733
Jose Carlos Cárdenas Martínez <josenanodev@gmail.com>
2024-03-13 13:23:58 -0600

    Refactor mutarArray function to use array.reverse()

--- servidor/acciones/mutarArray.js
+++ servidor/acciones/mutarArray.js
@@ -1,21 +1,6 @@
-function mutarArray(Array) {
-    const Arrayrraynuevo = [];
-    //for (let i = Arrayrray.legth; i < 0; i--);
-
-
-    function invertirArray(arr) {
-        const copiaArray = [...arr]; 
-        return copiaArray.reverse();
-    }
-
-
-    return Array
+function mutarArray(array) {
+  const arrayInvertido = array.reverse();
+  return arrayInvertido
 }
 
 module.exports = mutarArray;
-
-
-//for (let index = 0; index < array.length; index++) {
-    const element = array[index];
-    
-
