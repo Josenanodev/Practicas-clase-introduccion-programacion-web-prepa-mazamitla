d404b5f722b82ffeeff71006e2841e7477d1d1a5
Angelrac <157447547+Angelrac@users.noreply.github.com>
2024-03-13 14:31:30 -0600

    trabajo
    mis commits

---
+++ servidor/acciones/arrayMayus.js
@@ -0,0 +1,6 @@
+function mayuscularArray(array){
+    const arrayguardar = []
+    for (let i = 0; i < array.lenght; i++ ){
+        const 
+    }
+}
---
+++ servidor/acciones/mutarArray.js
@@ -0,0 +1,21 @@
+function mutarArray(array) {
+    //crear array
+    const arrayAgregar = [];
+    for (let i = 0; i < array.length; i++) {
+        const elemento = array[i];
+        const string = String(elemento); "hola " + "amigo"
+        arrayAgregar[i] = saludosumadomasuno;
+    }
+    return arrayAgregar
+}
+
+
+module.exports = mutarArray;
+
+//index = i
+
+//for (let index = 0; index < array.length; index++) {
+ //   const element = array[index];
+    
+//}
+
