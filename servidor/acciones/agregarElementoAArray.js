791c99143b128c24bbb996feaccc99e1f03423e3
Jose Carlos Cárdenas Martínez <josenanodev@gmail.com>
2024-03-19 13:53:15 -0600

    Fix bug in login functionality

---
+++ servidor/acciones/agregarElementoAArray.js
@@ -0,0 +1,13 @@
+//Entradas
+const array = ["uno", "dos", true]
+const nuevoElemento = 500
+
+//Salida 1
+const arrayNuevo = ["uno", "dos", true, 500]
+//Salida 2
+const arrayNuevo2 = [500, "uno", "dos", true]
+
+//Tip acceder a elemento de array
+array[numero] = valesAlgo
+
+
