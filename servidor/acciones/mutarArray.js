52e309e7ab1980acbddaf7d346e5653098457ccd
Jose Carlos Cárdenas Martínez <josenanodev@gmail.com>
2024-03-19 13:28:18 -0600

    Refactor convertirAMayusculas function to use map method

--- servidor/acciones/mutarArray.js
+++ servidor/acciones/mutarArray.js
@@ -1,12 +1,15 @@
 let arrayOriginal = ["hola", "mundo", "luminetz"];
 
 function convertirAMayusculas(array) {
-    let arrayNuevo = [];
-    for (let i = 0; i < array.length; i++) {
-        arrayNuevo[i] = array[i].toUpperCase();
-    }
-    return arrayNuevo;
+  // Metodo basico
+  // let arrayNuevo = [];
+  // for (let i = 0; i < array.length; i++) {
+  //     arrayNuevo[i] = array[i].toUpperCase();
+  // }
+  // return arrayNuevo;
+
+  // metodo con map
+  return array.map((elemento) => elemento.toUpperCase());
 }
 
-
-console.log("El array es:" + convertirAMayusculas(arrayOriginal))
+console.log("El array es:" + convertirAMayusculas(arrayOriginal));
