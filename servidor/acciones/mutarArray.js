b2b27d124745190ce2e3bb3ff524a334348c9da3
Jose Carlos Cárdenas Martínez <josenanodev@gmail.com>
2024-03-19 13:41:14 -0600

    Refactor mutarArray function to use reduce method

--- servidor/acciones/mutarArray.js
+++ servidor/acciones/mutarArray.js
@@ -1,15 +1,19 @@
 function mutarArray(array) {
-  //Creo mi nuevo array vacio
-
-  const cantidadDeValoresEnArray = array.length;
-  for (let index = cantidadDeValoresEnArray; index < cantidadDeValoresEnArray; index++) {
-    const elemento = array[index];
-
-  }
-  return sumaTotal;
+  // let sumaTotal = 0;
+  // for (let index = 0; index < array.length; index++) {
+  //   const elemento = array[index];
+  //   sumaTotal = sumaTotal + elemento;
+  // }
+  // return sumaTotal;
+  //   redeuce(funcionDelReduce, valorInicial)
+  //  funcionDelReduce(suma, elementoActual)
+  return array.reduce((suma, elementoActual) => suma + elementoActual, 0);
 }
 
-module.exports = mutarArray;
+const array = [1, 2, 3, 4, 5];
+console.log(mutarArray(array));
+
+// module.exports = mutarArray;
 
 //Angle String(valor)
 //David Invertir array con for
