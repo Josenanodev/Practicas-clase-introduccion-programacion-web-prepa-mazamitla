c0cf6709562e49bb85e43fa0cfef889384afcc69
Luminetz <romogodoyangel2023@gmail.com>
2024-03-20 13:26:00 -0600

    Correciones

--- servidor/acciones/agregarElementoAArray.js
+++ servidor/acciones/agregarElementoAArray.js
@@ -1,13 +1,13 @@
-const array = ["uno", "dos", "tres", true];
+const primerArray = ["uno", "dos", "tres", true];
 const nuevoElemento = 500;
 
-function agregarAlPrincipio(array, nuevoElemento) {
+function agregarAlPrincipio(array, Elemento) {
     for (let i = array.length; i > 0; i--) {
         array[i] = array[i - 1];
     }
     
-    array[0] = nuevoElemento;
+    array[0] = Elemento;
     return array;
 }
 
-console.log(agregarAlPrincipio(array, nuevoElemento));
+console.log(agregarAlPrincipio(primerArray, nuevoElemento));
