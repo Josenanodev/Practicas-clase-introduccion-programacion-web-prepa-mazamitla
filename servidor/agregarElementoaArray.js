c39c529db68e8b07945e4f6558f04b96216cc0e8
YueleT <yuelet666@gmail.com>
2024-03-20 14:17:06 -0600

    hola

---
+++ servidor/agregarElementoaArray.js
@@ -0,0 +1,30 @@
+// const array = ["uno", "dos",true]
+// const nuevoElemento = 500
+
+
+// const arrayNuevo = ["uno", "dos", true, 500]
+
+// const arrayNuevo2 = [500, "uno", "dos", true]
+
+// array[3] 
+
+
+
+
+
+// function agregarElementoAArray(array, elemtoNuevo){
+//     const longitudDeArray = array.length 
+//     array [longitudDeArray] = elemtoNuevo
+//     return array
+// }
+
+// const array = [1, "2", true, {}, "hola"]
+// const elemtoNuevo = "adios"
+// array.push(elemtoNuevo)
+// console.log(array)
+
+
+primerArray.unshift(nuevoElemento);
+ 
+console.log(primerArray.unshift(muevoElemento)); 
+
---
+++ servidor/reservacion.js
@@ -0,0 +1,18 @@
+let reservacion = {
+habitacion: "101",
+informacionDelHuesped: {
+    nombre: "angel adrian",
+    edad: 25,
+    identificacion: "1234567889",
+    numeroDeTelefono: "123456789",
+},
+fecha: {
+    confirmacio: "2015-03-07",
+    llegada: "2030-10-10",
+    salida: "2035-10-10",
+},
+informacion: {
+
+
+}
+}
