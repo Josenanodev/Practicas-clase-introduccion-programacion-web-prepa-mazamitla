4dc2a0a6073db222187453c63b6fe80c142359dd
Jose Carlos Cárdenas Martínez <josenanodev@gmail.com>
2024-03-20 13:26:55 -0600

    Update agregarElementoAArray.js

--- servidor/acciones/agregarElementoAArray.js
+++ servidor/acciones/agregarElementoAArray.js
@@ -1,13 +1,23 @@
-//Entradas
-const array = ["uno", "dos", true]
-const nuevoElemento = 500
+// //Entradas
+// const array = ["uno", "dos", true]
+// const nuevoElemento = 500
 
-//Salida 1
-const arrayNuevo = ["uno", "dos", true, 500]
-//Salida 2
-const arrayNuevo2 = [500, "uno", "dos", true]
+// //Salida 1
+// const arrayNuevo = ["uno", "dos", true, 500]
+// //Salida 2
+// const arrayNuevo2 = [500, "uno", "dos", true]
 
-//Tip acceder a elemento de array
-array[numero] = valesAlgo
+// //Tip acceder a elemento de array
+// array[numero] = valesAlgo
 
 
+function agregarElementoAArray(array, elementoNuevo){
+    const longitudDelArray = array.length
+    array[longitudDelArray] = elementoNuevo
+    return array
+}
+
+const array = [1, "2", true, {}, "hola", "😁"]
+const elementoNuevo = "adios"
+array.push(elementoNuevo)
+console.log(array)
